b9d0c3090dad801d0933ad99f77158d85b55969c
daliDeli <bruns8900@gmail.com>
2018-01-16 18:50:00 +0100

    working modal but not getting required data

--- src/components/CandidatesReport.js
+++ src/components/CandidatesReport.js
@@ -2,28 +2,32 @@ import React, { Component } from 'react';
 
 export default class CandidatesReport extends Component {
     constructor(props) {
-        super(props);   
+        super(props);
+    this.setAndSendModalState = this.setAndSendModalState.bind(this);
     }
 
+    setAndSendModalState() {
+        this.props.shouldOpenModal(true);   
+    }
     
-
     render() {
-
-        let report = this.props.renderReports;
+        
+        let { candidateName, companyName, interviewDate, status } = this.props.renderReports;
         return (
-            <table className="table table-bordered table-inverse text-center ">
-                <thead className=" container-fluid">
+            
+                
+                <tbody>
                     <tr className="row">
-                        <th className="col-12 col-md-1">#{this.props.id}</th>
-                        <th className="col-12 col-md-3">{report.candidateName}</th>
-                        <th className="col-12 col-md-3">{report.companyName}</th>
-                        <th className="col-12 col-md-2">{report.interviewDate}</th>
-                        <th className="col-12 col-md-1">{report.status}</th>
-                        <th className="col-6 col-md-1"><button type="button" className="btn btn-link"><i className='fa fa-eye' aria-hidden='true'></i></button></th>
-                        <th className="col-6 col-md-1"><button type="button" className="btn btn-link"><i className="fa fa-trash" aria-hidden="true"></i></button></th>
+                        <td className="col-12 col-md-1">#{this.props.id}</td>
+                        <td className="col-12 col-md-3">{candidateName}</td>
+                        <td className="col-12 col-md-3">{companyName}</td>
+                        <td className="col-12 col-md-2">{interviewDate}</td>
+                        <td className="col-12 col-md-1">{status}</td>
+                        <td className="col-6 col-md-1"><button type="button" onClick={this.setAndSendModalState} className="btn btn-link"><i className='fa fa-eye' aria-hidden='true'></i></button></td>
+                        <td className="col-6 col-md-1"><button type="button" className="btn btn-link"><i className="fa fa-trash" aria-hidden="true"></i></button></td>
                     </tr>
-                </thead>
-            </table>
+                </tbody>
+            
         )
     }
 }
